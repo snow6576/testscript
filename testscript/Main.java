49decafc0f5962902222b3331555390562201790
snow6576 <o_mukodai@outlook.jp>
2026-07-19 17:18:16 +0900

    a

--- testscript/Main.java
+++ testscript/Main.java
@@ -24,6 +24,7 @@ public class Main {
              Scanner scanner = new Scanner(System.in);
 
              while (true){
+                 System.out.print(">>");
                  testScript.run(scanner.next());
              }
          }
